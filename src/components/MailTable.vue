63ad728e84117c81c7167b397b38dc7540929ba5
Kesab Nayak <nayak@keeunit.de>
2023-03-15 15:20:25 +0100

    jsonserver

---
+++ src/components/MailTable.vue
@@ -0,0 +1,34 @@
+<template>
+   <table class="mail-table">
+    <tbody>
+        <tr v-for="email in emails"
+        :key="email.id"
+        :class="['clickable',email.read ? 'read':'']"
+        @click="email.read= true"
+        >
+        <td><input type="checkbox"/></td>
+        <td>{{ email.from }}</td>
+        <td><p><strong>{{ email.subject }}</strong> - {{ email.body }}</p></td>
+        <!-- <td class="date">{{format(new Date(email.sentAt), 'MMM do yyyy')}}</td> -->
+        <td class="date">{{ email.sentAt }}
+        </td>
+    </tr>
+    </tbody>
+</table>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    async  setup(){
+        let { data: emails } = await axios.get('http://localhost:3000/emails')
+        return {
+         emails,
+        }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
